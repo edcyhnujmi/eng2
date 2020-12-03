ed35da644f671cecc379b5e469d31203d483b6d2
edcyhnujmi <43102478+edcyhnujmi@users.noreply.github.com>
2020-12-04 00:23:28 +0900

    Update sentencesCheck.py

--- sentencesCheck.py
+++ sentencesCheck.py
@@ -38,7 +38,7 @@ if 'id' in form:
         senK.close()
 
         listStrF = '\n'
-        numSen = 401 #문장 개수
+        numSen = 406 #문장 개수
         with open("senEng", encoding='UTF8') as fileE:
                 for i, line in enumerate(fileE):
                     if i < numSen :
