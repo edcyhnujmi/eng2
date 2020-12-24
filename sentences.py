c6ce8a257176a6b8366629f839b30d9f0ccac52a
edcyhnujmi <43102478+edcyhnujmi@users.noreply.github.com>
2020-12-24 14:38:08 +0900

    Add files via upload

--- sentences.py
+++ sentences.py
@@ -38,7 +38,7 @@ if 'id' in form:
         senK.close()
 
         listStrF = '\n'
-        numSen = 419 #문장 개수에 따라 수정필요함!
+        numSen = 426 #문장 개수에 따라 수정필요함!
         with open("senEng", encoding='UTF8') as fileE:
                 for i, line in enumerate(fileE):
                     if i < numSen :
